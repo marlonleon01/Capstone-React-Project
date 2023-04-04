7c33463e8ba39c20133b9dba5958c7d25300ad35
Marlon Leon <ghostveene@gmail.com>
2023-04-04 11:42:18 -0400

    passing photos state to context provider

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const Context = createContext()
 
 function ContextProvider(props) {
+    const [allPhotos, setAllPhotos] = useState([])
+    
     return (
-        <Context.Provider value={""}>
+        <Context.Provider value={{allPhotos}}>
             {props.children}
         </Context.Provider>
     )
