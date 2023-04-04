a6f88de392a413ed1960a33aa49265509c7fc525
Marlon Leon <ghostveene@gmail.com>
2023-04-04 12:35:37 -0400

    conditionally render heart and carticon on images during hover

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -3,6 +3,9 @@ import { useState } from "react"
 export default function Image(props) {
     const [hovered, setHovered] = useState(false)
 
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
         <div className={`${props.className} image-container`}>
             <img 
@@ -11,6 +14,8 @@ export default function Image(props) {
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}
             />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
