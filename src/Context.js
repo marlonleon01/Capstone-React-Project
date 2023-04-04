ae703f0560a74f4afcda87d1cbae6729d4c0268f
Marlon Leon <ghostveene@gmail.com>
2023-04-04 11:36:32 -0400

    created context and exported to index.js

---
+++ src/Context.js
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const Context = createContext()
+
+function ContextProvider(props) {
+    return (
+        <Context.Provider value={""}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
