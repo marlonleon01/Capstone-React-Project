3b3a04a725e62ab73702e248c087146e0d85865a
Marlon Leon <ghostveene@gmail.com>
2023-04-04 11:49:38 -0400

    fetching photos array and saving to state on render

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const Context = createContext()
 
 function ContextProvider(props) {
+    const photosUrl = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
     const [allPhotos, setAllPhotos] = useState([])
-    
+
+    useEffect(() => {
+        fetch(photosUrl)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+        
     return (
         <Context.Provider value={{allPhotos}}>
             {props.children}
