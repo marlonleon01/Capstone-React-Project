a66dddbf5514b9ce6b80e657ea6bac02c32af75b
Marlon Leon <ghostveene@gmail.com>
2023-04-04 13:46:45 -0400

    added toggleFavorite function

--- src/Context.js
+++ src/Context.js
@@ -12,8 +12,23 @@ function ContextProvider(props) {
             .then(data => setAllPhotos(data))
     }, [])
         
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if (photo.id === id) {
+                console.log(id)
+                console.log(!photo.isFavorite)
+                return {
+                    ...photo,
+                    isFavorite: !photo.isFavorite
+                }
+            }
+            return photo
+        })
+        setAllPhotos(updatedArr)
+    }
+
     return (
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {props.children}
         </Context.Provider>
     )
