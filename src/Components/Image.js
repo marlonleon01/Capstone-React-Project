34e84ecb9437dd0bd3437d6ccb138616b8d42ee4
Marlon Leon <ghostveene@gmail.com>
2023-04-04 12:23:15 -0400

    changing state on hover of img

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export default function Image(props) {
+    const [hovered, setHovered] = useState(false)
+
     return (
         <div className={`${props.className} image-container`}>
-            <img src={props.img}  className="image-grid"/>
+            <img 
+                src={props.img} 
+                className="image-grid"
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            />
         </div>
     )
 }
