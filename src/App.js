81345d0e78e8094bd6e11ef3198eb6bf3a90e806
Marlon Leon <ghostveene@gmail.com>
2023-04-04 11:14:23 -0400

    added routes from cart and photos components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import Header from "./Components/Header"
+import Cart from "./Pages/Cart"
+import Photos from "./Pages/Photos"
+import {Routes, Route} from "react-router-dom"
+
 export default function App() {
 	return (
-		<div></div>
+		<div>
+			<Header />
+			<h1>Home page</h1>
+			<Routes>
+				<Route path="/" element={<Photos />}></Route>
+				<Route path="/cart" element={<Cart />}></Route>
+			</Routes>
+		</div>
 	)
 }
