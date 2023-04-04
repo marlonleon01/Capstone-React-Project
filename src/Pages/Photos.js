b8f61eaa7cba288a1f622338d9e9be4151eb9087
Marlon Leon <ghostveene@gmail.com>
2023-04-04 12:13:10 -0400

    loading photos from array stored in state

--- src/Pages/Photos.js
+++ src/Pages/Photos.js
@@ -1,7 +1,18 @@
+import { useContext } from "react"
+import { Context } from "../Context"
+import Image from "../Components/Image"
+import {getClass} from "../utils"
+
 export default function Photos() {
+    const {allPhotos} = useContext(Context)
+
+    const newAllPhotosArr = allPhotos.map((photos, index) => (
+        <Image key={photos.id} img={photos.url} className={getClass(index)}/>
+    ))
+
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {newAllPhotosArr}
         </main>
     )
 }
