1f99cc97b639116a49ca6f7f723e2cd3004ab162
Marlon Leon <ghostveene@gmail.com>
2023-04-05 10:33:36 -0400

    removing from cart checkout page

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,8 +1,12 @@
+import { useContext } from "react"
+import { Context } from "../Context"
+
 export default function CartItem({item}) {
-    console.log(item)
+    const {removeImageFromCart} = useContext(Context)
+    
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
+            <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
