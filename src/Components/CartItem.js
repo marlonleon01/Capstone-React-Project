93f954acce793594f10cdb234c00a650f20518f2
Marlon Leon <ghostveene@gmail.com>
2023-04-05 10:24:28 -0400

    adding img to cart page

---
+++ src/Components/CartItem.js
@@ -0,0 +1,10 @@
+export default function CartItem({item}) {
+    console.log(item)
+    return (
+        <div className="cart-item">
+            <i className="ri-delete-bin-line"></i>
+            <img src={item.url} width="130px" />
+            <p>$5.99</p>
+        </div>
+    )
+}
