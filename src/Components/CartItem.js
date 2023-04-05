d87c53689173918a826a4f0aeb2867a1dd21081e
Marlon Leon <ghostveene@gmail.com>
2023-04-05 11:21:25 -0400

    rendering filled icon when hovering over it

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,12 +1,26 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { Context } from "../Context"
 
 export default function CartItem({item}) {
     const {removeImageFromCart} = useContext(Context)
+    const [hovered, setHovered] = useState(false)
     
+    function trashIcon() {
+        if (hovered) {
+            return "ri-delete-bin-fill"
+        } else {
+            return "ri-delete-bin-line"
+        }
+    }
+
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
+            <i 
+                className={trashIcon()} 
+                onClick={() => removeImageFromCart(item.id)}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            ></i>
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
