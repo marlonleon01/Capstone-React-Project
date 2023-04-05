f5fe34e4ca9962dcd5fc8c582dd730fe8466ff05
Marlon Leon <ghostveene@gmail.com>
2023-04-05 11:00:17 -0400

    order being placed

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,9 +1,10 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import {Context} from "../Context"
 import CartItem from "../Components/CartItem"
 
 export default function Cart() {
-    const {cartItems} = useContext(Context)
+    const {cartItems, setCartItems} = useContext(Context)
+    const [orderPlaced, setOrderPlaced] = useState(false)
     const totalCost = 5.99 * cartItems.length
     const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
 
@@ -11,13 +12,23 @@ export default function Cart() {
         <CartItem key={item.id} item={item} />
     ))
 
+    function placeOrder() {
+        setOrderPlaced(true)
+        setTimeout(() => {
+            setCartItems([])
+            setOrderPlaced(false)
+        }, 3000)
+    }
+
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
             <p className="total-cost">Total: {totalCostDisplay}</p>
             <div className="order-button">
-                <button>Place Order</button>
+                <button onClick={() => placeOrder()}>
+                    {orderPlaced? "Ordering..." : "Place Order"}
+                </button>
             </div>
         </main>
     )
