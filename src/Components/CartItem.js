98ed570758be2aca686e4c52a7d1388d11ad52d8
Marlon Leon <ghostveene@gmail.com>
2023-04-05 10:51:03 -0400

    removed toLocaleString

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -4,14 +4,11 @@ import { Context } from "../Context"
 export default function CartItem({item}) {
     const {removeImageFromCart} = useContext(Context)
     
-    const number = 5.99
-    const pricePerItem = number.toLocaleString("en-US", {style: "currency", currency: "USD"})
-
     return (
         <div className="cart-item">
             <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
             <img src={item.url} width="130px" />
-            <p>{pricePerItem}</p>
+            <p>$5.99</p>
         </div>
     )
 }
