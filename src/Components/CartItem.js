425055b324b1ff7663a3e147788784395fa782fb
Marlon Leon <ghostveene@gmail.com>
2023-04-05 10:47:39 -0400

    using tolocalestring to display price of each item need to add total

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -4,11 +4,14 @@ import { Context } from "../Context"
 export default function CartItem({item}) {
     const {removeImageFromCart} = useContext(Context)
     
+    const number = 5.99
+    const pricePerItem = number.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
     return (
         <div className="cart-item">
             <i className="ri-delete-bin-line" onClick={() => removeImageFromCart(item.id)}></i>
             <img src={item.url} width="130px" />
-            <p>$5.99</p>
+            <p>{pricePerItem}</p>
         </div>
     )
 }
